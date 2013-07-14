c8fa967046ea1308c9be1aa946ca93d16b39a380
Shu Xu <shu.xu94@gmail.com>
2013-07-14 17:36:05 -0400

    added autopilot methods

--- autopilot.py
+++ autopilot.py
@@ -2,18 +2,21 @@ class Autopilot(object):
 	def __init__(self, submarine):
 		self.submarine = submarine
 
-	def goHere(self, location): #  location is a gps object
-		
-	
-	def goHere(self, location[]):
+	def goHere(self, location):
+		self.desx = location.x
+		self.dexy = location.y				 #  location is a gps object
 	
 	def goHere(self, x, y):
 		self.desx = x
 		self.desy = y
 	
 	def maintainSpeed(self, speed):
-	
+		self.desiredSpeed = speed
+		self.currentSpeed = self.submarine.sensor.getLastestSpeed()
+		
 	def maintainHeading(self, heading):
+		self.desiredHeading = heading
+		self.currentHeading = self.sumbarine.sensor.getLatestHeading()
 	
 	def setBase(self, coord):
 		self.basex = coord.x

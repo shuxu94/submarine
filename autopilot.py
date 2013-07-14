8ef799fca2e830a91c8323cd54e9c7c081bdc99f
Shu Xu <shu.xu94@gmail.com>
2013-07-13 21:41:10 -0400

    first commit

---
+++ autopilot.py
@@ -0,0 +1,24 @@
+class Autopilot(object):
+	def __init__(self, submarine):
+		self.submarine = submarine
+
+	def goHere(self, location): #  location is a gps object
+		
+	
+	def goHere(self, location[]):
+	
+	def goHere(self, x, y):
+		self.desx = x
+		self.desy = y
+	
+	def maintainSpeed(self, speed):
+	
+	def maintainHeading(self, heading):
+	
+	def setBase(self, coord):
+		self.basex = coord.x
+		self.basey = coord.y
+	
+	def setBase(self, x, y):
+		self.basex = x
+		self.basey = y
---
+++ commands.py
@@ -0,0 +1,44 @@
+import communication
+import sensor
+
+#  the command is sent in the following format
+#  motor, elevator, rudder
+
+
+baudrate = 9600
+timeout = 2
+class Submarine(object):
+	def __init__(self, serial, sensor):
+		self.serial = serial #  communication.serial object
+		self.sensor = sensor #  sensor.sensor object 
+				
+	def sendCommand(self):
+		self.controlmessage = "%d, %d, %d\n" % (
+												self.motorsetting,
+												self.elevatorsetting,
+												self.ruddersetting)
+												
+		self.serial.sendMessage(self.controlmessage)
+		
+	def setAllControls(self, motorsetting, elevatorsetting, ruddersetting):
+		self.motorsetting = motorsetting	
+		self.elevatorsetting = elevatorsetting
+		self.ruddersetting = ruddersetting
+		
+		self.sendCommand()
+		
+	def setMotor(self, motorSetting):
+ 		self.motorsetting = motorsetting
+ 		
+		self.sendCommand()
+		
+		
+	def setElevator(self, elevatorSetting):
+		self.elevatorsetting = elevatorsetting
+		
+		self.sendCommand()
+	
+	def setRudder(self, rudderSetting):
+		self.rudderSetting = rudderSetting
+		
+		self.sendCommand()

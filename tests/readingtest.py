b9c129dd36bf3fe8dbf1bd56116711705854181e
Shu Xu <shu.xu94@gmail.com>
2013-07-16 14:47:31 -0400

    updated readingtest.py

--- tests/readingtest.py
+++ tests/readingtest.py
@@ -3,14 +3,8 @@ import sensor
 import commands
 import serial
 import time
+import devicepath
 
 
-testserial = serial.Serial(communication.ShusMacdevicePath1, 9600, timeout = 2)
-
-	stop = time.time()+3
-	while time.time() < stop:
-		data = testserial.readline().split()
-		if len(data) == 5:
-			print "gpsxy = %f %f" % (float(data[0]), float(data[1]))
-			print "compassxy = %f %f" % (float(data[2]), float(data[3]))
-			print "tempurature = %f" % float(data[4])
+test serial = communication.Serial(Shusmac1, 9600, 2):
+	print getMessage

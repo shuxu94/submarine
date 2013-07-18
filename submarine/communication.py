491eb9b80be1d3f724598b3c55e3c6515b619263
Shu Xu <shu.xu94@gmail.com>
2013-07-18 19:36:10 -0400

    added basic functions to socket classes

--- submarine/communication.py
+++ submarine/communication.py
@@ -1,19 +1,47 @@
 import serial
 import socket
 import thread
+import sensor
 import time
 import devicepath
+import sys
 
 #  Sensor data coming in in the following form:
 #  x-coordinate, y-coordinate, compassx, compassy, temperature
 
-class PIsocket(socket.socket):
-	pass
-	
+class PIsocket(socket.socket): #  PIsocket is the server side 
+							   #  User datagram 
+	def __init__(self, ip, port):
+		super(PIsocket, self).__init__(socket.AF_INET, socket.SOCK_DGRAM)
+		self.ip = ip
+		self.port = port
+		try:
+			self.bind((self.ip, self.port))
+		except socket.error, msg:
+			print 'socket error'
+			sys.exit()
+	def sendMessage(self, message):
+		s.sendto(message, (self.ip, self.port))
+	def getMessage(self):
+		self.data, self.addr = self.recvfrom(1024)
+		
+		
 class Compsocket(socket.socket):
-	pass
+	def __init__(self, ip, port):
+		super(PIsocket, self).__init__(socket.AF_INET, socket.SOCK_DGRAM)
+		self.ip = ip
+		self.port = port
+		try:
+			self.bind((self.ip, self.port))
+		except socket.error, msg:
+			print 'socket error'
+			sys.exit()
+	def sendMessage(self, message):
+		s.sendto(message, (self.ip, self.port))
 
-	
+	def getMessage(self):
+		self.data, self.addr = self.recvfrom(1024)
+		
 class Serial(object):
 	def __init__(self, devicepath, brate, tout):
 		self.Serial = serial.Serial(devicepath, baudrate = brate, timeout = tout)

a107d3dbf7ee3e41a9b683fa0f747dacb6e91cf0
Sayan Roychowdhury <sayan.royc@gmail.com>
2013-07-18 15:30:08 -0400

    ardcode update
    takes inputs and outputs them

--- ArdCode/ArdCode.ino
+++ ArdCode/ArdCode.ino
@@ -1,5 +1,24 @@
 #include <Servo.h> // motor control library
 
+#define COMPXMIN -490.0// used to calibrate magnetometer readings into heading
+#define COMPXMAX 55.0
+#define COMPYMIN -328.0
+#define COMPYMAX 140.0
+
+#define MOTORMAX 1810 //less than max backward
+#define MOTORMIN 1140 //less than max forward
+//#define MOTORMAX 1900 //max backward
+#define MOTORMID 1475 //stopped
+//#define MOTORMIN 1050 //max forward
+#define MOTORMAXCHANGE 2 //max angle change per cycle
+
+#define RUDDERMAX 2200 //right
+#define RUDDERMID 1400 //straight
+#define RUDDERMIN 900 //left
+
+#define ELEVATORMAX 2150
+#define ELEVATORMID 1515
+#define ELEVATORMIN 880
 
 String serialDataIn;
 char serialInput;
@@ -9,6 +28,9 @@ char serialInput;
 int motor_pwm;
 int elevator_pwm;
 int rudder_pwm;
+int motor_pwm_old = MOTORMID;
+int elevator_pwm_old = ELEVATORMID;
+int rudder_pwm_old = RUDDERMID;
 //int inbyte;
 
 Servo Motor;
@@ -29,17 +51,18 @@ void setup(){
 
 void loop() 
 { 
-    serialDataIn = "";
-    readfromPI();
-  
+      serialDataIn = "";
+      readfromPI();
+      
     Serial.println(motor_pwm);
     Serial.println(elevator_pwm);
     Serial.println(rudder_pwm);
-/*    
+    delay(3000);
+    
     Motor.write(motor_pwm);              // tell servo to go to position in variable 'pos' 
     Elevator.write(elevator_pwm);
     Rudder.write(rudder_pwm);
-*/
+
 } 
 
 void readfromPI()
@@ -63,8 +86,8 @@ void readfromPI()
     while(Serial.available()){
        serialInput = Serial.read();
        serialDataIn.concat(serialInput);
-    }   
-    
+    }
+
     // printing out the string that has been received from serial in the format #,#,#
 /*    if (serialDataIn != ""){
         Serial.println(serialDataIn);
@@ -79,8 +102,31 @@ void readfromPI()
     
      motor_pwm= motorpwm.toInt();
      elevator_pwm= elevatorpwm.toInt();
-     rudder_pwm = rudderpwm.toInt();
-
+     rudder_pwm = rudderpwm.toInt();     
+     
+     
+     // if there is no new input (aka inputs are 0), then go back to old input
+     if(motor_pwm == 0){
+       motor_pwm = motor_pwm_old;
+     }
+     else{
+       motor_pwm_old = motor_pwm;
+     }
+     
+     if(elevator_pwm == 0){
+       elevator_pwm = elevator_pwm_old;
+     }
+     else{
+       elevator_pwm_old = elevator_pwm;
+     }
+     
+     if(rudder_pwm == 0){
+       rudder_pwm = rudder_pwm_old;
+     }
+     else{
+       rudder_pwm_old = rudder_pwm;
+     }
+     
 }
 
 

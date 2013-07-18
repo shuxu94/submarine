4041cf60515fa0b2ae47018de5537b5b4caf2784
Shu Xu <shu.xu94@gmail.com>
2013-07-18 11:38:11 -0400

    adding a threading module

---
+++ tests/communication_tests.py
@@ -0,0 +1,10 @@
+from nose.tools import *
+from submarine import communication
+from submarine import devicepath
+
+
+##  needs to be connect to arduino to work
+def test_serial():
+	#ardserial = communication.Serial(devicepath.Shusmac1, 9600, 2)
+	print "hello, test"
+	

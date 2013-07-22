4a3d83de4482c33522221175e2fd73aba902def6
Shu Xu <shu.xu94@gmail.com>
2013-07-22 13:10:28 -0400

    host changed to pi

--- submarine/startComp.py
+++ submarine/startComp.py
@@ -1,6 +1,6 @@
 import communication
 import time
-host = 'localhost'
+host = '192.168.234.234'
 port = 8888
 
 socket = communication.Compsocket(host,port)
@@ -8,6 +8,6 @@ socket = communication.Compsocket(host,port)
 while 1:
 	time.sleep(2)
 	msg = str(time.time())
-	print 'not socket' + msg
+	print 'sending: ' + msg
 	socket.sendMessage(msg)
 	

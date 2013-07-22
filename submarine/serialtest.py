31030e27a54593fd145bfe2525eee92f678f78fb
Shu Xu <shu.xu94@gmail.com>
2013-07-22 14:30:33 -0400

    anything

--- submarine/serialtest.py
+++ submarine/serialtest.py
@@ -1,6 +1,6 @@
 import communication
 import devicepath
 
-testserial = communication.Serial(devicepath.Pi, 9600, 2)
+testserial = communication.Serial(devicepath.Shusmac1, 9600, 2)
 while 1:
 	testserial.sendMessage('1600,1600,1600')

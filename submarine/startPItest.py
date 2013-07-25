0ed2618b4cd03f9044eec1601f788df59c2efe27
Shu Xu <shu.xu94@gmail.com>
2013-07-25 14:38:10 -0400

    added tests

---
+++ submarine/startPItest.py
@@ -0,0 +1,36 @@
+import communication
+import time
+import submarine
+import devicepath
+
+host = ''
+port = 8888
+baud = 9600
+timeout = 2
+oldmessage = ''
+
+socket = communication.PIsocket(host, port)
+serial = communication.Serial(devicepath.Pi, baud, timeout)
+sub1 = submarine.Submarine(serial)
+controlReciever = communication.Reciever()
+
+socket.start()
+try:
+	listen = communication.ListeningThread(socket)
+	listen.daemon=True
+	listen.start()
+	listen.addReciever(controlReciever)
+
+except (KeyboardInterrupt, SystemExit):
+	print '\n! Received keyboard interrupt, quitting threads.\n'
+
+while 1:
+	if controlReciever.message == None:
+		continue
+	if controlReciever.message == oldmessage:
+		continue
+	else:
+		message = controlReciever.message
+		print message
+		sub1.serial.sendMessage(message)
+		oldmessage = message
